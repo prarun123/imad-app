42224490c0dda74d5a984626bdccd81d2bd548dd
prarun123 <30665201+prarun123@users.noreply.github.com>
2017-08-15 20:20:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,68 @@
-console.log('Loaded!');
+var button = document.getElementById('counter1');
+var count = document.getElementById('count');
+button.onclick = function () {
+	var request = new XMLHttpRequest();
+
+	request.onreadystatechange = function (){
+		if (request.readyState === XMLHttpRequest.DONE ){
+			if (request.status === 200) {
+				var counter = request.responseText;
+				count.innerHTML = counter.toString();
+			}
+		}
+	};
+
+	request.open('GET','http://localhost/counter',true);
+	request.send(null);
+}
+
+
+var sub = document.getElementById('sub');
+var ul = document.getElementById('ul');
+sub.onclick = function () {
+	var request = new XMLHttpRequest();
+	ul.innerHTML = '';
+	request.onreadystatechange = function (){
+		if (request.readyState === XMLHttpRequest.DONE ){
+			if (request.status === 200) {
+				var names = request.responseText;
+				names = JSON.parse(names);
+				for (var i=0; i<names.length; i++)
+				ul.innerHTML += "<li>" + names[i] + "</li>";
+			}
+		}
+	};
+	var source1 = document.getElementById('source');
+	var source = source1.value;
+	// request.open('GET','http://localhost/get_name/'+ source,true);
+	request.open('GET','http://localhost/get_name/?name='+ source,true);
+	request.send(null);
+}
+
+
+
+
+// console.log('Loaded!');
+
+// alert ("javascript alert box");
+
+// var element = document.getElementById('main-text');
+
+// element.innerHTML = "New Text";
+
+// var image = document.getElementById('madi');
+
+// var marginL = 0;
+
+// function move(){
+// 	marginL = marginL + 10;
+// 	image.style.marginLeft = marginL + 'px';
+// }
+
+// image.onclick = function () {
+// 	// marginL = marginL + 10;
+// 	// image.style.marginLeft = marginL + 'px';
+// 	// console.log(image.style.marginLeft);
+// 		var interval = setInterval(move, 50);
+// }
+
