ea7ec0287cdd42b34ad2ebc333670b0fab21d322
prarun123 <30665201+prarun123@users.noreply.github.com>
2017-08-20 22:40:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,8 +93,6 @@ var config = {
     port: '5432',
     password: process.env.DB_PASSWORD
 };
-
-
 var pool = new Pool(config);
 app.get('/test-db', function(req,res) {
    pool.query('SELECT * FROM article', function(err,result) {
